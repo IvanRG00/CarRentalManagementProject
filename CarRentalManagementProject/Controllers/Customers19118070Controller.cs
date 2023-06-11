2fe4b42d48de75247bbe20e0e5d65db6ec850ab2
Ivan Georgiev <ivan402200@abv.bg>
2023-06-11 12:32:39 +0300

    -Added Excel export -Improved overall view

--- CarRentalManagementProject/Controllers/Customers19118070Controller.cs
+++ CarRentalManagementProject/Controllers/Customers19118070Controller.cs
@@ -8,6 +8,8 @@ using Microsoft.EntityFrameworkCore;
 using CarRentalManagementProject.Models;
 using Microsoft.AspNetCore.Authorization;
 using System.Data;
+using ClosedXML.Excel;
+using System.IO;
 
 namespace CarRentalManagementProject.Controllers
 {
@@ -167,5 +169,55 @@ namespace CarRentalManagementProject.Controllers
         {
           return (_context.Customers19118070s?.Any(e => e.CustomerId == id)).GetValueOrDefault();
         }
+        public async Task<IActionResult> ExportToExcel1()
+        {
+            var customers = await _context.Customers19118070s
+                .Include(c => c.RentedCar)
+                .ToListAsync();
+
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("Customers");
+
+                // Set header row
+                worksheet.Cell(1, 1).Value = "Customer ID";
+                worksheet.Cell(1, 2).Value = "First Name";
+                worksheet.Cell(1, 3).Value = "Middle Name";
+                worksheet.Cell(1, 4).Value = "Last Name";
+                worksheet.Cell(1, 5).Value = "Rented Car ID";
+
+                // Populate data rows
+                for (int i = 0; i < customers.Count; i++)
+                {
+                    var customer = customers[i];
+                    worksheet.Cell(i + 2, 1).Value = customer.CustomerId;
+                    worksheet.Cell(i + 2, 2).Value = customer.FirstName;
+                    worksheet.Cell(i + 2, 3).Value = customer.MiddleName;
+                    worksheet.Cell(i + 2, 4).Value = customer.LastName;
+                    worksheet.Cell(i + 2, 5).Value = customer.RentedCarId;
+                }
+
+                // Auto-fit columns
+                worksheet.Columns().AdjustToContents();
+
+                // Save the workbook to a byte array
+                byte[] excelData;
+                using (var stream = new MemoryStream())
+                {
+                    workbook.SaveAs(stream);
+                    excelData = stream.ToArray();
+                }
+
+                // Create a new MemoryStream from the byte array
+                var fileStream = new MemoryStream(excelData);
+                var fileName = "Customers.xlsx";
+                var contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+                return File(fileStream, contentType, fileName);
+            }
+        }
+
+
+
+
     }
 }
